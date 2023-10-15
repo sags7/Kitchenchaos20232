f212799b69a40369fce45d319b12fe2b2906a70e
Juan Sebastian Aramburo <juansebastianaramburo@gmail.com>
2023-10-15 22:54:28 +1100

    Added LookAtScript for UI progress bar

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+
+    [SerializeField] Mode _mode;
+    private void LateUpdate()
+    {
+        switch (_mode)
+        {
+            case Mode.LookAt:
+                Vector3 directionFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + directionFromCamera);
+                break;
+
+            case Mode.LookAtInverted:
+                transform.LookAt(Camera.main.transform);
+                break;
+
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+
+        }
+    }
+}
