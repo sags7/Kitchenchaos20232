5f2d64f9cba1b9e1099ccd6eee388a8a39608f93
Juan Sebastian Aramburo <juansebastianaramburo@gmail.com>
2023-10-18 23:34:23 +1100

    Changed the array for a list on DishSpawnerCounter
    Also cleaned up that script a bit.

--- Assets/Scripts/Counters/DishSpawnerCounter.cs
+++ Assets/Scripts/Counters/DishSpawnerCounter.cs
@@ -1,55 +1,57 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DishSpawnerCounter : BaseCounter
 {
     [SerializeField] private int _maxPlates = 4;
-    [SerializeField] private KitchenWieldable[] _currentPlates;
-    private float _SpawnTimer = 0;
-    [SerializeField] private float _spawnDelay = 2;
+    [SerializeField] private float _spawnDelay = 1;
     [SerializeField] private GameObject _plate;
+    private List<KitchenWieldable> _currentPlates;
+    private float _SpawnTimer = 0;
 
     private void Start()
     {
         _SpawnTimer = 0;
+        _currentPlates = new List<KitchenWieldable>();
     }
     private void Update()
     {
-        if (_currentPlates.Length < _maxPlates)
+        if (_currentPlates.Count < _maxPlates)
         {
             _SpawnTimer += Time.deltaTime;
             if (_SpawnTimer >= _spawnDelay)
             {
                 _SpawnTimer = 0;
-                AddToArr(SpawnKitchenWieldable());
+                AddToList(SpawnKitchenWieldable());
             }
         }
-        if (_currentPlates.Length > 0) KitchenWieldableHeld = _currentPlates[_currentPlates.Length - 1];
     }
     public override void Interacted(IKitchenWieldableParent player)
     {
-        if (!player.KitchenWieldableHeld)
+        if (!player.KitchenWieldableHeld && _currentPlates.Count > 0)
         {
             SwapWieldablesWith(player);
-            if (_currentPlates.Length >= 2) KitchenWieldableHeld = _currentPlates[_currentPlates.Length - 2];
-            if (_currentPlates.Length > 0) RemoveLastFromArr();
+            RemoveLastFromList();
+            if (_currentPlates.Count != 0) KitchenWieldableHeld = _currentPlates[^1];
+            else KitchenWieldableHeld = null;
         };
     }
 
-    private void RemoveLastFromArr()
+    private void RemoveLastFromList()
     {
-        Array.Resize(ref _currentPlates, _currentPlates.Length - 1);
+        _currentPlates.RemoveAt(_currentPlates.Count - 1);
     }
 
-    private void AddToArr(KitchenWieldable kitchenWieldable)
+    private void AddToList(KitchenWieldable kitchenWieldable)
     {
-        Array.Resize(ref _currentPlates, _currentPlates.Length + 1);
-        _currentPlates[^1] = kitchenWieldable;
+        _currentPlates.Add(kitchenWieldable);
+        KitchenWieldableHeld = kitchenWieldable;
     }
 
     private KitchenWieldable SpawnKitchenWieldable()
     {
-        float _spawnOffset = _currentPlates.Length * 0.1f;
+        float _spawnOffset = _currentPlates.Count * 0.1f;
         GameObject newPlate = Instantiate(_plate, SpawnPoint.transform.position + new Vector3(0, _spawnOffset, 0), SpawnPoint.rotation);
         return newPlate.GetComponent<KitchenWieldable>();
     }
