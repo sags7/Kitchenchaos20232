d0dcf30e45b0764f471d67adf8f2429e1d39f7c0
Juan Sebastian Aramburo <juansebastianaramburo@gmail.com>
2023-10-23 22:54:24 +1100

    Changed the foreach on the delivery manager for a for loop
    Still getting ObjectDisposedException:SerializedProperty
    on either method

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class DeliveryManager : MonoBehaviour
 {
     [SerializeField] private DeliveryCounter _deliveryCounter;
-    [SerializeField] public List<DishRecipeSO> _queuedOrders;
+    private List<DishRecipeSO> _queuedOrders;
     [SerializeField] private List<DishRecipeSO> _possibleOrders;
-    private float _newOrderTimer;
-    private float _orderInterval = 1.5f;
+    [SerializeField] private float _orderInterval = 5f;
+    private float _newOrderTimer = 0;
     private int _maxQueue = 5;
 
     private void Start()
@@ -18,16 +18,37 @@ public class DeliveryManager : MonoBehaviour
         _deliveryCounter.OnPlateDelivered += OnPlateDeliveredAction;
     }
 
+    // private void OnPlateDeliveredAction(object sender, DeliveryCounter.OnPlateDeliveredEventArgs args)
+    // {
+    //     Plate plate = args._plateDelivered;
+    //     bool recipeFound = false;
+    //     foreach (DishRecipeSO recipe in _queuedOrders)
+    //     {
+    //         if (recipeFound == false && plate.ExtractListOfRequirements(plate._heldItems).SequenceEqual(plate.ExtractListOfRequirements(recipe)))
+    //         {
+    //             Debug.Log("<color=green> Correct Recipe!</color>");
+    //             _queuedOrders.Remove(recipe);
+    //             args._plateDelivered.DestroySelf();
+    //             recipeFound = true;
+    //             _newOrderTimer = 0;
+    //             break;
+    //         }
+    //     }
+    //     if (recipeFound == false) Debug.Log("<color=red>Incorrect Recipe!!!</color>");
+    // }
     private void OnPlateDeliveredAction(object sender, DeliveryCounter.OnPlateDeliveredEventArgs args)
     {
         bool recipeFound = false;
-        foreach (DishRecipeSO order in _queuedOrders)
+        for (int i = 0; i < _queuedOrders.Count; i++)
         {
-            if (recipeFound == false && args._plateDelivered.ExtractListOfRequirements(args._plateDelivered._heldItems).SequenceEqual(args._plateDelivered.ExtractListOfRequirements(order)))
+            if (recipeFound == false && args._plateDelivered.ExtractListOfRequirements(args._plateDelivered._heldItems).SequenceEqual(args._plateDelivered.ExtractListOfRequirements(_queuedOrders[i])))
             {
                 Debug.Log("<color=green> Correct Recipe!</color>");
+                _queuedOrders.RemoveAt(i);
                 args._plateDelivered.DestroySelf();
                 recipeFound = true;
+                _newOrderTimer = 0;
+                break;
             }
         }
         if (recipeFound == false) Debug.Log("<color=red>Incorrect Recipe!!!</color>");
@@ -37,11 +58,12 @@ public class DeliveryManager : MonoBehaviour
     {
         _newOrderTimer += Time.deltaTime;
 
-        if (_newOrderTimer >= _orderInterval && _queuedOrders.Count <= _maxQueue)
+        if (_newOrderTimer >= _orderInterval && _queuedOrders.Count < _maxQueue)
         {
             _newOrderTimer = 0;
             _queuedOrders.Add(_possibleOrders[Random.Range(0, _possibleOrders.Count)]);
             foreach (DishRecipeSO order in _queuedOrders) Debug.Log(order._orderName);
         }
+        if (_newOrderTimer >= _orderInterval) _newOrderTimer = 0;
     }
 }
